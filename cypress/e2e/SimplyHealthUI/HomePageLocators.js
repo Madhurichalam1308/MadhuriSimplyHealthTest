098b05843a22f86962aa457caa09cd5ca3294610
madhurichalam <madhuri.chalam@so.energy>
2023-06-17 10:58:35 +0100

    updated locators

---
+++ cypress/e2e/SimplyHealthUI/HomePageLocators.js
@@ -0,0 +1,9 @@
+export class HomePageLocators {
+
+    getAHealthPlan = "#button1148957918-1";
+    dentalPlan = "body > div.root.responsivegrid > div > div.site-nav.aem-GridColumn.aem-GridColumn--default--12 > header > div.sh-site-header__inner > nav > div.sh-nav__main__nav > ul > li.sh-nav__tier-1__item.selected > ul > li:nth-child(2) > a";
+    healthPlan = "strong[aria-label = 'Label for submenus of Health plans']";
+    myAccount = "body > div.root.responsivegrid > div > div.site-nav.aem-GridColumn.aem-GridColumn--default--12 > header > div.sh-site-header__inner > div > div > a"
+}
+
+export const homePageLocators = new HomePageLocators();
