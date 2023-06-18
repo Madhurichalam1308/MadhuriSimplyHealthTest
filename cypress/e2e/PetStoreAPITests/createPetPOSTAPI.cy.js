bf0aa9b3d84ffc0272e3b1373ab9e72f108a4cda
madhurichalam <madhuri.chalam@so.energy>
2023-06-18 15:14:20 +0100

    Pet CRUD update

--- cypress/e2e/PetStoreAPITests/createPetPOSTAPI.cy.js
+++ cypress/e2e/PetStoreAPITests/createPetPOSTAPI.cy.js
@@ -1,6 +1,6 @@
-describe('Create and add a new pet to the store',() =>
+describe('Create  a new pet , Get the pet details by id and finally delete the created pet ',() =>
 {
-    it('Add and get details of the new Pet',()=> {
+    it('Add details of the new Pet',()=> {
         cy.fixture('createPet').then((payload) => {
             cy.request({
 
@@ -28,11 +28,35 @@ describe('Create and add a new pet to the store',() =>
 
             }).then((res) => {
                 expect(res.status).to.eq(200)
-                expect(res.body.id).to.eq(payload.id)
+            })
+
+
+        })
+    })
+    it('get sdetails by pet id', () => {
+        cy.fixture('createPet').then((payload) => {
+            cy.request({
+                method: 'GET',
+                url: Cypress.env('getPetUrl')
+            }).then((res) => {
+                expect(res.status).to.eq(200)
+                expect(res.body.id).to.eq(1090)
                 expect(res.body.name).to.eq(payload.name)
                 expect(res.body.status).to.eq(payload.status)
             })
         })
     })
+    it('Delete a pet by ID', () => {
+        cy.request({
+            method: 'DELETE',
+            url: Cypress.env('deletePetUrl'),
+            headers :{
+                'api_key':'special-key'
+            }
 
+        }).then((res) => {
+            expect(res.status).to.eq(200)
+
+        })
+    })
 })
